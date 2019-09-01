90dea092563078f545d31a5b5d914437e268a4c6
Elliot Jordan <elliot@elliotjordan.com>
2019-09-01 16:35:02 -0700

    Make basic urlopen() calls Python 3 friendly

--- Processors/AdobeAcrobatDcUpdateInfoProvider.py
+++ Processors/AdobeAcrobatDcUpdateInfoProvider.py
@@ -21,9 +21,13 @@
 # pylint: disable=e1101
 
 from __future__ import absolute_import
-import urllib2
 
 from autopkglib import Processor, ProcessorError
+
+try:
+    from urllib.parse import urlopen  # For Python 3
+except ImportError:
+    from urllib2 import urlopen  # For Python 2
 
 __all__ = ["AdobeAcrobatDcUpdateInfoProvider"]
 
@@ -69,10 +73,8 @@ class AdobeAcrobatDcUpdateInfoProvider(Processor):
     def get_reader_updater_dmg_url(self, major_version):
         '''Returns download URL for Adobe Reader Updater DMG'''
 
-        request = urllib2.Request(
-            AR_UPDATER_BASE_URL + AR_URL_TEMPLATE % major_version)
         try:
-            url_handle = urllib2.urlopen(request)
+            url_handle = urlopen(AR_UPDATER_BASE_URL + AR_URL_TEMPLATE % major_version)
             version_string = url_handle.read()
             url_handle.close()
         except BaseException as err:
